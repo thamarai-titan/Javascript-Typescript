0830a5420461cb8998b3b9a75d189cfe9ead5cd1
Thamarai <tmanalan710@gmail.com>
2026-03-17 21:48:10 +0530

    map function modified

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ console.log("hello from Index.js" )
 // Map function 
 arr = [1,2,3,4,5]
 
-console.log(arr.map((item) => {
-  console.log(item)
-}))
+arr.map((item) => {console.log(item)})
+
+// Filter function
+
+arr.filter(x => x%2 === 0)
